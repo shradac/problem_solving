7d73398b12220872c6211a52d5e692a6fa8a2217
Shrada C <56028645+shradac@users.noreply.github.com>
2022-01-18 17:14:37 +0530

    Create problem3sum.py

---
+++ problem3sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result=[]
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i]==nums[i-1]: continue
+            left=i+1
+            right=len(nums)-1
+            
+            while left<right:
+                currentsum= nums[i]+nums[left]+nums[right]
+                
+                if currentsum==0:
+                    result.append([nums[i],nums[left],nums[right]])
+                    
+                    while left<right and nums[left]==nums[left+1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right-1]:
+                        right-=1
+                        
+                    left+=1
+                    right-=1
+                    
+                elif currentsum<0:
+                        left+=1
+                else:
+                    right-=1
+                    
+        return result
